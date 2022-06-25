f149df1f3e619c6d0ea3f4aa03c4ece8222a14e5
smishra <suman.mishra@greenwayhealth.com>
2022-06-26 02:55:36 +0530

    B-104 Changed the provider configuration

--- aws.tf
+++ aws.tf
@@ -1,19 +1,15 @@
 terraform {
-  required_providers {
-    aws = {
-      source = "hashicorp/aws"
-      version = "4.20.1"
-    }
-    /* you can use a remote backend / or standard backend like s3 
-if using s3 enable versioning and encryption for storing the tf states"
-*/
 
-  }
+  /* you can use a remote backend / or standard backend like s3 
+    if using s3 enable versioning and encryption for storing the tf states"
+    */
+
 }
 
+
 provider "aws" {
-  profile = "saml"
-  region = "us-east-1"
-  shared_credentials_files = "C:\\Users\\smishra\\.aws\\credentials"
+  profile                 = "saml"
+  region                  = "us-east-2"
+  shared_credentials_file = "C:\\Users\\smishra\\.aws\\credentials"
 }
 /* we have a lock file for the state as well*/
